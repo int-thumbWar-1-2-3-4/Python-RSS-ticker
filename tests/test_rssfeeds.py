78d4f60610cff586c8436fd725bf4e3adc91e5ce
James Coleman <jcolem22@msudenver.edu>
2020-03-02 20:36:57 -0700

    Enabled the test file to see the file it was testing and then wrote a test for rssFeedDemoModule

--- tests/test_rssfeeds.py
+++ tests/test_rssfeeds.py
@@ -1,4 +1,15 @@
-from .context import rs
+import sys
 
-def test_answer():
-    assert rs.rssFeedDemoModule() == 'inside rss Feed Demo Module'
+sys.path.append('/Users/jamescoleman/PycharmProjects/Python-RSS-ticker/')
+
+import unittest
+from rssfeeds import feed as rs
+
+class TestRSSFeed(unittest.TestCase):
+
+    def test_answer(self):
+        assert rs.rssFeedDemoModule() == 'inside rss Feed Demo Module'
+
+
+if __name__== '__main__':
+    unittest.main()
