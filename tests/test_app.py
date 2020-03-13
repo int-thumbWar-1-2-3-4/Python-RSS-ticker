fdb1dd0d74a1983461c6a7c72a667ed782d99003
James Coleman <jcolem22@msudenver.edu>
2020-03-12 21:30:32 -0600

    I deleted test_app contents for the sake of our collective repo, I have the code on my local machine though

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,4 @@
 import unittest
-import sys
-import os
-
-directory = os.path.dirname(__file__)
-relativePath = directory[0: len(directory) - 5]
-
-sys.path.append(relativePath)
-
-
 from tkinter import Tk
 from View import app 
 
