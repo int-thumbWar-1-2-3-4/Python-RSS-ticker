4431bad273b6526de03391396f2fd8485bfbf51c
James Coleman <jcolem22@msudenver.edu>
2020-03-12 20:50:56 -0600

    test_app not working quite right. Probably needs mocking

--- View/app.py
+++ View/app.py
@@ -2,14 +2,14 @@ from tkinter import *
 
 # root window created. Here, that would be the only window, but
 # you can later have windows within windows.
-import userinterface
+import View.userinterface as ui
 
 root = Tk()
 
 root.geometry("400x300")
 
 # creation of an instance
-app = userinterface.Window(root)
+app = ui.Window(root)
 
 # mainloop
 root.mainloop()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,13 @@
 import unittest
+import sys
+import os
+
+directory = os.path.dirname(__file__)
+relativePath = directory[0: len(directory) - 5]
+
+sys.path.append(relativePath)
+
+
 from tkinter import Tk
 from View import app 
 
