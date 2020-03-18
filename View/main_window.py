1cda8b11c8cb948d2ad0894bc228cf97a05c7b36
Brendan Bassett <bbasset3@msudenver.edu>
2020-03-18 15:51:10 -0600

    removed blank __init__.py file

--- View/main_window.py
+++ View/main_window.py
@@ -4,6 +4,8 @@ import time
 
 class Application(tk.Frame):
 
+    articles = {}
+
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
@@ -11,22 +13,19 @@ class Application(tk.Frame):
         self.pack()
 
         self.load_articles()
-        self.create_widgets()
+        self.create_widget()
 
-    articles = {}
 
-    def create_widgets(self):
+    def create_widget(self):
         self.content_label = tk.Label(self)
         self.content_label["text"] = list(self.articles.keys())[0]
         self.content_label.pack(side="top")
 
 
     def load_articles(self):
-        d = feedparser.parse('https://www.theguardian.com/world/rss')
-        for entry in d.entries:
+        feed = feedparser.parse('https://www.theguardian.com/world/rss')
+        for entry in feed.entries:
             self.articles[entry.title] = entry.link
-
-
 
 root = tk.Tk()
 app = Application(master=root)
