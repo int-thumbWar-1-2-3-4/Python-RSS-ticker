648e0e2bf09f3dab8639f4486fbd3d7e338a48a4
James Coleman <jcolem22@msudenver.edu>
2020-03-18 17:50:21 -0600

    I tried moving code from controller/__init__ to a new 'start.py' file to see if that is what was making Travis angry

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,6 +1,1 @@
-import tkinter as tk
-from view.main_window import MainWindow
 
-root = tk.Tk()
-app = MainWindow(master=root)
-app.mainloop()
---
+++ controller/start.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from view.main_window import MainWindow
+
+root = tk.Tk()
+app = MainWindow(master=root)
+app.mainloop()
