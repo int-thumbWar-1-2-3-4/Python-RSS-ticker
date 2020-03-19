079467835fdc93e1c468892ce950eb3367e01414
Brendan Bassett <bbasset3@msudenver.edu>
2020-03-18 19:49:22 -0600

    Fixing start.py

--- controller/start.py
+++ controller/start.py
@@ -1,6 +1,14 @@
 import tkinter as tk
+
+from model.model import Model
 from view.main_view import MainView
 
 root = tk.Tk()
-app = MainView(master=root)
-app.mainloop()
+mainView = MainView(master=root)
+
+model = Model(mainView)
+model.load_entries("https://www.theguardian.com/us/rss")
+model.switch_displayed_entry()
+
+# KEEP THIS LAST
+mainView.mainloop()
