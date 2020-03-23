3ed25c133e4ff6e9cc275c52c370d5894e6fb530
Brendan Bassett <bbasset3@msudenver.edu>
2020-03-23 13:34:28 -0600

    added model_tests.py for unit testing model.py

---
+++ tests/model_tests.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class ModelTestCase(unittest.TestCase):
+
+    # TODO: Model.load_entries() test
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    # TODO: Model.switch_displayed_entry() test
+    def switch_displayed_entry(self):
+        self.assertEqual(True, False)
+
+    # TODO: Model.is_empty() test
+    def is_empty(self):
+        self.assertEqual(True, False)
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_all.md
+++ tests/test_all.md
@@ -2,5 +2,5 @@
 
 #### To run all the tests at once navigate to python-RSS-ticker and
 #### enter 'python3 -m unittest discover tests/' inside your terminal.
-#### Alternativly you can enter 'coverage run -m unittest discover tests/' 
-#### and get simillar results.
+#### Alternatively you can enter 'coverage run -m unittest discover tests/' 
+#### and get similar results.
