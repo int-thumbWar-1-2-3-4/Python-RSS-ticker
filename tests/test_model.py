eee60fd13e604f144caf8ef6698befaf7b82b1ff
Brendan Bassett <bbasset3@msudenver.edu>
2020-03-23 13:41:00 -0600

    renamed test_model.py and test functions

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,16 +4,17 @@ import unittest
 class ModelTestCase(unittest.TestCase):
 
     # TODO: Model.load_entries() test
-    def test_something(self):
+    def test_load_entries(self):
         self.assertEqual(True, False)
 
     # TODO: Model.switch_displayed_entry() test
-    def switch_displayed_entry(self):
+    def test_switch_displayed_entry(self):
         self.assertEqual(True, False)
 
     # TODO: Model.is_empty() test
-    def is_empty(self):
+    def test_is_empty(self):
         self.assertEqual(True, False)
+
 
 if __name__ == '__main__':
     unittest.main()
