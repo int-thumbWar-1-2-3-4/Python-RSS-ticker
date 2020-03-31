436047825913b8d41b33859b8f922be4211a8cfd
James Coleman <jcolem22@msudenver.edu>
2020-03-31 16:05:47 -0600

    controller.title_loop is now tested. Coverage says 100% but I don't buy it

--- tests/test_title_loop.py
+++ tests/test_title_loop.py
@@ -1,12 +1,22 @@
 import unittest
-from controller.title_loop import ten_second_loop, call_switch_display
-
+from unittest.mock import patch
+from controller import title_loop
 
 class TestTitleLoop(unittest.TestCase):
 
-    def test_ten_second_loop(self):
-        pass
+    @patch('controller.title_loop.th.Timer')
+    @patch('view.main_view')
+    @patch('controller.title_loop.call_switch_display')
+    def test_ten_second_loop(self, mock_timer, mock_main_view, mock_switch_display):
+        title_loop.mv = mock_main_view
+        title_loop.t = 7
+        title_loop.ten_second_loop(title_loop.mv, title_loop.t)
+        self.assertTrue(mock_timer.called)
+        # title_loop.ten_second_loop.assert_called_with(mock_main_view, 7)
+        self.assertTrue(mock_switch_display.called)
 
-    def test_call_switch_display(self):
-        
-        pass
+    @patch('view.main_view')
+    def test_call_switch_display(self, mock_main_view):
+        title_loop.test_feed = [('test title', "test url")]
+        title_loop.call_switch_display(mock_main_view)
+        mock_main_view.display_entry.assert_called_with('test title', "test url")
