466b19f6ea087ae36ae37f89ac9288a99c673bfe
James Coleman <jcolem22@msudenver.edu>
2020-03-31 16:09:56 -0600

    I merged my test branch with my loop branch and added a some comments

--- tests/test_title_loop.py
+++ tests/test_title_loop.py
@@ -2,12 +2,14 @@ import unittest
 from unittest.mock import patch
 from controller import title_loop
 
+
 class TestTitleLoop(unittest.TestCase):
 
     @patch('controller.title_loop.th.Timer')
     @patch('view.main_view')
     @patch('controller.title_loop.call_switch_display')
     def test_ten_second_loop(self, mock_timer, mock_main_view, mock_switch_display):
+        """ Unit test of controller.title_loop.ten_second_loop """
         title_loop.mv = mock_main_view
         title_loop.t = 7
         title_loop.ten_second_loop(title_loop.mv, title_loop.t)
@@ -17,6 +19,7 @@ class TestTitleLoop(unittest.TestCase):
 
     @patch('view.main_view')
     def test_call_switch_display(self, mock_main_view):
+        """ Unit test of controller.title_loop.call_switch_display """
         title_loop.test_feed = [('test title', "test url")]
         title_loop.call_switch_display(mock_main_view)
         mock_main_view.display_entry.assert_called_with('test title', "test url")
