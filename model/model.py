b884f32c67f09aae947483e2b02726ee2e392131
Brendan Bassett <bbasset3@msudenver.edu>
2020-04-01 14:28:29 -0600

    Reworking model from a TDD perspective

--- model/model.py
+++ model/model.py
@@ -1,59 +1,109 @@
 import feedparser
 
 from view.main_view import MainView
+from typing import List
 
 
-class Model():
-    # This class loads the feed entries and stores them in a dictionary of title, link pairs.
+class Article:
 
-    # The entries currently loaded  from the feed
-    __feed_titles = []
-    __feed_entries = {}
-    __displayed_entry_title = "[BLANK Entry Title]"
+    def __init__(self, title: str, link: str, date: str):
+        self.title = title
+        self.link = link
+        self.date = date
 
-    def __init__(self, main_view: MainView):
-        self.__main_view = main_view
 
-    def load_entries(self, feed_link: str):
-        # Loads all of the entries from the feed at the specified location.
+class Feed:
 
-        feed = feedparser.parse(feed_link)
-        for entry in feed.entries:
-            self.__feed_titles.append(entry.title)
-            self.__feed_entries[entry.title] = entry.link
+    def __init__(self, name: str):
+        self.__list_of_articles = List[Article]
+        self.__position = -1    # -1 here means the list of articles is empty
 
-        if not self.is_empty():
-            first_title = self.__feed_titles[0]
-            self.__main_view.display_entry(first_title, self.__feed_entries.get(first_title))
+        self.name = name
 
-    def switch_displayed_entry(self):
-        # Finds the next entry's title and link and prompts the view to display it.
-        print('switched_displayed_entry')
-        # Output an error message if the dictionary is empty and skip the rest of the method.
-        if self.is_empty():
-            print("ERROR The dictionary of feed entries is empty!")
-            self.__main_view.display_entry("BLANK Title", "BLANK Link")
-            return
+    def sort(self):
+        # TODO: Fill in Feed.sort() method
+        pass
 
-        # Search the dictionary of feed entries for the one that is currently displayed.
-        for index, entry in enumerate(self.__feed_entries, start=0):
+    def update(self, new_list_of_articles: List[Article]):
+        # TODO: Fill in Feed.update() method
+        pass
 
-            # If this entry is the last one in the dictionary, display the first title, link pair.
-            if (index + 1) == len(self.__feed_entries):
-                first_title = self.__feed_titles[0]
-                self.__main_view.display_entry(first_title, self.__feed_entries.get(first_title))
+    def get_next(self) -> Article:
+        # TODO: Fill in Feed.get_next() method
+        pass
 
-            # The entry is not at the end, so see if this title matches the one being displayed.
-            #   Display the next title, link pair in the dictionary.
-            elif entry.title == self.__displayed_entry_title:
-                next_title = self.__feed_titles[index + 1]
-                self.__displayed_entry_title = next_title
-                self.__main_view.display_entry(next_title, self.__feed_entries.get(next_title))
+    def add(self, new_article: Article):
+        # TODO: Fill in Feed.add() method
+        pass
 
-    def is_empty(self):
-        # Determines whether the dictionary of entries is empty
 
-        if len(self.__feed_titles) == 0:
-            return True
-        else:
-            return False
+class Model:
+
+    def __init__(self):
+        self.__list_of_feeds = List[Feed]
+        self.__current_feed: Feed = None
+
+    def add(self, new_list_of_articles: List[Article]) -> bool:
+        # TODO: Fill in Model.add(list) method
+        return False
+
+    def add(self, new_article: Article) -> bool:
+        # TODO: Fill in Model.add(article) method
+        return False
+
+    def remove(self, feedName: str) -> bool:
+        # TODO: Fill in Model.remove() method
+        return False
+
+    # This class stores articles from rss or atom feeds and orders them from newest to oldest. When multiple feeds are
+    # #   loaded, they will alternate between each feed with each feed ordered internally from newest to oldest
+    #
+    # # The articles currently loaded  from the feed
+    # __collection_of_articles = OrderedDict()
+    # __index_of_current_article = -1     # -1 indicates that the collection is empty
+    #
+    # def __init__(self, main_view: MainView):
+    #     self.__main_view = main_view
+    #
+    # def load_entries(self, feed_link: str):
+    #     # Loads all of the entries from the feed at the specified location.
+    #
+    #     feed = feedparser.parse(feed_link)
+    #     for entry in feed.entries:
+    #         self.__collection_of_articles[entry.title] = entry.link
+    #
+    #     if not self.is_empty():
+    #         first_title = self.__feed_titles[0]
+    #         self.__main_view.display_entry(first_title, self.__collection_of_articles.get(first_title))
+    #
+    # def switch_displayed_entry(self):
+    #     # Finds the next entry's title and link and prompts the view to display it.
+    #     print('switched_displayed_entry')
+    #     # Output an error message if the dictionary is empty and skip the rest of the method.
+    #     if self.is_empty():
+    #         print("ERROR The dictionary of feed entries is empty!")
+    #         self.__main_view.display_entry("BLANK Title", "BLANK Link")
+    #         return
+    #
+    #     # Search the dictionary of feed entries for the one that is currently displayed.
+    #     for index, entry in enumerate(self.__collection_of_articles, start=0):
+    #
+    #         # If this entry is the last one in the dictionary, display the first title, link pair.
+    #         if (index + 1) == len(self.__collection_of_articles):
+    #             first_title = self.__feed_titles[0]
+    #             self.__main_view.display_entry(first_title, self.__collection_of_articles.get(first_title))
+    #
+    #         # The entry is not at the end, so see if this title matches the one being displayed.
+    #         #   Display the next title, link pair in the dictionary.
+    #         elif entry.title == self.__displayed_article_title:
+    #             next_title = self.__feed_titles[index + 1]
+    #             self.__displayed_article_title = next_title
+    #             self.__main_view.display_entry(next_title, self.__collection_of_articles.get(next_title))
+    #
+    # def is_empty(self):
+    #     # Determines whether the dictionary of entries is empty
+    #
+    #     if len(self.__collection_of_articles) == 0:
+    #         return True
+    #     else:
+    #         return False
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,19 +1,59 @@
 import unittest
+from model.model import Article, Feed, Model
+from unittest.mock import Mock
 
 
 class ModelTestCase(unittest.TestCase):
 
-    # TODO: Model.load_entries() test
-    def test_load_entries(self):
-        self.assertEqual(True, False)
+    def test_article(self):
+        title = "title"
+        link = "link"
+        date = "date"
 
-    # TODO: Model.switch_displayed_entry() test
-    def test_switch_displayed_entry(self):
-        self.assertEqual(True, False)
+        article = Article(title, link, date)
 
-    # TODO: Model.is_empty() test
-    def test_is_empty(self):
-        self.assertEqual(True, False)
+        self.assertEqual(article.title, title)
+        self.assertEqual(article.link, link)
+        self.assertEqual(article.date, date)
+
+    def test_feed(self):
+        feed_name = "test feed"
+
+        feed = Feed(feed_name)
+
+        self.assertEqual(feed.name, feed_name)
+
+    def test_feed_sort(self):
+        # TODO: Create test for Feed.sort()
+        pass
+
+    def test_feed_update(self):
+        # TODO: Create test for Feed.update()
+        pass
+
+    def test_feed_get_next(self):
+        # TODO: Create test for Feed.get_next()
+        pass
+
+    def test_feed_add(self):
+        # TODO: Create test for Feed.add()
+        pass
+
+    def test_model(self):
+        # TODO: Create test for Model
+        pass
+
+    def test_model_add_list(self):
+        # TODO: Create test for Model.add(list_of_articles)
+        pass
+
+    def test_model_add_article(self):
+        # TODO: Create test for Model.add(article)
+        pass
+
+    def test_model_remove(self):
+        # TODO: Create test for Model.remove()
+        pass
 
 
 if __name__ == '__main__':
