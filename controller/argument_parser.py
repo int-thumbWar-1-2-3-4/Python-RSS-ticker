dad40d1280de2b0572ff63c74193c685fbdebeb1
James Coleman <jcolem22@msudenver.edu>
2020-04-08 18:02:53 -0600

    I refactored the controller module so that start.py is once again the file that you can start with.

--- controller/argument_parser.py
+++ controller/argument_parser.py
@@ -1,24 +1,19 @@
 import argparse
-from controller.start import execute
-from model.model import parse
 
-parser = argparse.ArgumentParser(description="Select a file or feed to parse.")
-parser.add_argument('--url', dest='url', action='store', default="",
-                    help="enter a url of an RSS or ATOM feed to parse", nargs='*')
-parser.add_argument('--file', dest='file', action='store', default="",
-                    help="enter a file name to parse", nargs='*')
-parser.add_argument('--config', dest='config', action='store', default='',
-                    help="optionally enter a .yaml config file", nargs='*')
-parser.add_argument('--timer', dest='timer', action='store', type=int, default=10)
-args = parser.parse_args()
 
-feed = parse(args.url[0])
-feed.reverse()
-seconds = 10
+def ticker_argument_parser():
+    parser = argparse.ArgumentParser(description="Select a file or feed to parse.")
+    parser.add_argument('--url', dest='url', action='store', default="",
+                        help="enter a url of an RSS or ATOM feed to parse", nargs='*')
+    parser.add_argument('--file', dest='file', action='store', default="",
+                        help="enter a file name to parse", nargs='*')
+    parser.add_argument('--config', dest='config', action='store', default='',
+                        help="optionally enter a .yaml config file", nargs='*')
+    parser.add_argument('--timer', dest='timer', action='store', type=int, default=10)
+    return parser.parse_args()
 
-if args.timer is not None:
-    seconds = args.timer
 
-execute(feed, seconds)
+
+
 
 
