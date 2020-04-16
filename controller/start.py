4ae9d36000e0fac152aa149a2e358f50601a327b
James Coleman <jcolem22@msudenver.edu>
2020-04-15 18:51:04 -0600

    refactored controller.start

--- controller/start.py
+++ controller/start.py
@@ -6,16 +6,19 @@ from view.main_view import MainView
 from model.parser import parse_url_feed
 from model.feed_manager import parse
 
-
-if __name__ == "__main__":
+def main():
     arguments = ticker_argument_parser()
     feed = parse(arguments.url[0])
     feed.reverse()
+    ten_second_loop(mainView, arguments.timer, feed)
 
+
+
+if __name__ == "__main__":
     root = tk.Tk()
     mainView = MainView(master=root)
 
-    ten_second_loop(mainView, arguments.timer, feed)
+    main()
 
     # KEEP THIS LAST
     mainView.mainloop()
