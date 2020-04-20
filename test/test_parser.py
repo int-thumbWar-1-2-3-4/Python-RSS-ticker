295be71acba49238fb06d4b8306aff0006f8cca9
Brendan Bassett <bbasset3@msudenver.edu>
2020-04-20 16:21:22 -0600

    Added test_parser_files

--- test/test_parser.py
+++ test/test_parser.py
@@ -4,20 +4,13 @@ from unittest.mock import Mock
 import model.parser as parser
 
 
-sys.modules['atoma'] = Mock()
-sys.modules['requests'] = Mock()
-sys.modules['lxml'] = Mock()
-sys.modules['bs4'] = Mock()
-
-
 # @patch('parser.bs4.BeautifulSoup')
-class test_parser(unittest.TestCase):
+class TestParser(unittest.TestCase):
 
     def test_get_multi_feed_contents(self):
         # TODO: Create a test for get_multi_feed_contents()
         pass
 
     def test_get_feed_contents(self):
-        # TODO: Create a test for get_feed_contents()
+        parser.get_feed_contents("C:/test.rss.xml")
         pass
-
