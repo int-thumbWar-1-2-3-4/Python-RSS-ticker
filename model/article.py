9fbb75ef703a05164ec18ec5edc9cb41eba1cf1e
James Coleman <jcolem22@msudenver.edu>
2020-04-23 10:01:06 -0600

    All non test functions now have a log debug output (except the logger function its self)

--- model/article.py
+++ model/article.py
@@ -1,10 +1,17 @@
 from datetime import datetime
+from controller.utilities import logger
+
+
+a_logger = logger('model.article')
 
 class Article:
     # TODO: Split Article class into its own file
     # A single rss item, i.e. a single news article.py
 
     def __init__(self, title: str, link: str, published_date: datetime):
+
+        a_logger.debug('Article.__init__')
+        
         self.title = title
         self.link = link
         self.published_date = published_date
