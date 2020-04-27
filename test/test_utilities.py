0fd9bc1967bd67f5c252bc91c49aa56072dbdaad
James Coleman <jcolem22@msudenver.edu>
2020-04-26 22:06:53 -0600

    Cleaned up code in test_utilities and view/main_view

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -7,21 +7,25 @@ from controller.utilities import logger, ticker_argument_parser
 
 
 class TestUtilities(unittest.TestCase):
-	""" Test class for controller.utilities """
+    """ Test class for controller.utilities. """
 
-	def test_logger(self):
-		""" Unit test for controller.utilities.logger. Should return a Logger object """
-		
-		result = logger('name')
-		self.assertTrue(isinstance(result, lg.Logger))
+    def test_logger(self):
+        """
+        Unit test for controller.utilities.logger. Should return a Logger
+        bject.
+        """
+        result = logger('name')
+        self.assertTrue(isinstance(result, lg.Logger))
 
 
 class TestArgumentParser(unittest.TestCase):
     """ Test class for tests.test_argument_parser. """
 
     def test_has_each_argument(self):
-        """ Unit test for controller.utilities.ticker_argument_parser. Are all args present? """
-
+        """
+        Unit test for controller.utilities.ticker_argument_parser. Are all
+        args present?
+        """
         sys.argv = ['PRSST']
         args = ticker_argument_parser()
         self.assertTrue('url' in args)
@@ -30,8 +34,10 @@ class TestArgumentParser(unittest.TestCase):
         self.assertTrue('timer' in args)
 
     def test_url(self):
-        """ Unit test for controller.utilities.ticker_argument_parser's url argument. """
-
+        """
+        Unit test for controller.utilities.ticker_argument_parser's url
+        argument.
+        """
         fake_url = ['www.fakeurl.com']
         sys.argv = ['test', '--url', fake_url[0]]
         args = ticker_argument_parser()
@@ -40,8 +46,10 @@ class TestArgumentParser(unittest.TestCase):
         self.assertEqual(args.url, fake_url)
 
     def test_file(self):
-        """ Unit test for controller.utilities.ticker_argument_parser's file argument. """
-
+        """
+        Unit test for controller.utilities.ticker_argument_parser's
+        file argument.
+        """
         fake_file = ['fakefile.txt']
         sys.argv = ['test', '--file', fake_file[0]]
         args = ticker_argument_parser()
@@ -50,8 +58,10 @@ class TestArgumentParser(unittest.TestCase):
         self.assertEqual(args.file, fake_file)
 
     def test_logger(self):
-        """ Unit test for controller.utilities.ticker_argument_parser's logger argument. """
-
+        """
+        Unit test for controller.utilities.ticker_argument_parser's
+        logger argument.
+        """
         fake_logger = ['WARNING']
         sys.argv = ['test', '--logger', fake_logger[0]]
         args = ticker_argument_parser()
@@ -61,8 +71,10 @@ class TestArgumentParser(unittest.TestCase):
         self.assertEqual(args.logger, fake_logger)
 
     def test_timer(self):
-        """ Unit test for controller.utilities.ticker_argument_parser's timer argument. """
-
+        """
+        Unit test for controller.utilities.ticker_argument_parser's
+        timer argument.
+        """
         sys.argv = ['news ticker', '--timer', '17']
         args = ticker_argument_parser()
 
@@ -71,8 +83,10 @@ class TestArgumentParser(unittest.TestCase):
         self.assertEqual(args.timer, 17)
 
     def test_default_timer(self):
-        """ Unit test for controller.utilities.ticker_argument_parser's default timer value. """
-
+        """
+        Unit test for controller.utilities.ticker_argument_parser's
+        default timer value.
+        """
         sys.argv = ['this is the prog field (the name of the program']
         args = ticker_argument_parser()
 
@@ -81,26 +95,32 @@ class TestArgumentParser(unittest.TestCase):
         self.assertEqual(args.timer, 10)
 
     def test_all_args(self):
-        """ Unit test for controller.utilities.ticker_argument_parser testing the handling of all arguments at once."""
-
+        """
+        Unit test for controller.utilities.ticker_argument_parser testing
+        the handling of all arguments at once.
+        """
         # TODO, finish this test. The argument needs to be expanded
 
         url = "www.notasite.com"
-        time = 13
+        time = '13'
         file = 'some/file.txt'
         logger = 'ERROR'
-        sys.argv = ['test', '--url', url]
+        sys.argv = ['test', '-u', url, '-t', time, '-f', file, '-l', logger]
         args = ticker_argument_parser()
         self.assertEqual(args.url[0], url)
         # self.assertEqual(args.time, 13)
 
 
 class TestTickerArgumentParser(unittest.TestCase):
-    """ Test class to test the creation of the argument parser """
+    """ Test class to test the creation of the argument parser. """
 
     @patch('controller.utilities.argparse.ArgumentParser')
-    def test_calls_argparse_function_argument_parser(self, mock_argument_parser):
-        """ Unit test for ArgumentParser, checks that our argument parser was created correctly """
+    def test_calls_argparse_function_argument_parser(self, mock_parser):
+        """
+        Unit test for ArgumentParser, checks that our argument parser was
+        reated correctly.
+        """
+        description = "Select a file or feed to parse."
         ticker_argument_parser()
-        mock_argument_parser.assert_called_with(description="Select a file or feed to parse.",
-                                                fromfile_prefix_chars='@')
+        mock_parser.assert_called_with(description=description,
+                                       fromfile_prefix_chars='@')
--- view/main_view.py
+++ view/main_view.py
@@ -7,8 +7,10 @@ mv_logger = logger('view.main_view')
 
 
 class MainView(tk.Frame):
-    """ Class for view.main_view creates, displays, modifies and receives input from the user interface """
-
+    """
+    Class for view.main_view creates, displays, modifies and receives
+    input from the user interface
+    """
     entry_title = "Welcome to Tiny Ticker news feed"
     entry_link = "https://github.com/int-thumbWar-1-2-3-4/Python-RSS-ticker"
 
@@ -27,13 +29,17 @@ class MainView(tk.Frame):
         self.menu_bar()
 
     def menu_bar(self):
-        """ View.main_view.MainView.menu_bar adds a drop down menu for our tk window. """
-
+        """
+        View.main_view.MainView.menu_bar adds a drop down menu for our
+        tk window.
+        """
         mv_logger.debug('MainView.menubar')
 
-        color = ["red", "green", "blue", "yellow", "cyan", "magenta", "white", "black"]
-        fonts = ['times 8', 'times 10', 'times 12', 'times 14', 'times 16', 'times 18', 
-                 'times 20', 'times 24', 'times 26', 'times 28', 'times 36', 'times 48']
+        color = ["red", "green", "blue", "yellow", "cyan", "magenta", "white",
+                 "black"]
+        fonts = ['times 8', 'times 10', 'times 12', 'times 14', 'times 16',
+                 'times 18', 'times 20', 'times 24', 'times 26', 'times 28',
+                 'times 36', 'times 48']
 
         self.menubar = tk.Menu(self)
         self.dropdown_menu = tk.Menu(self.menubar, tearoff=0)
@@ -41,12 +47,14 @@ class MainView(tk.Frame):
         self.font_menu = tk.Menu(self.menubar)
 
         for c in color:
-            self.dropdown_menu.add_command(label=c, command=lambda c=c: self.change_window('bg', c))
-            self.font_color.add_command(label=c, command=lambda c=c: self.change_window('fg', c))
-
+            self.dropdown_menu.add_command(label=c, command=lambda c=c:
+                                           self.change_window('bg', c))
+            self.font_color.add_command(label=c, command=lambda c=c:
+                                        self.change_window('fg', c))
 
         for f in fonts:
-            self.font_menu.add_command(label=f, command=lambda f=f: self.change_window('font', f))
+            self.font_menu.add_command(label=f, command=lambda f=f:
+                                       self.change_window('font', f))
 
         self.menubar.add_cascade(label='bg color', menu=self.dropdown_menu)
         self.menubar.add_cascade(label='Font size', menu=self.font_menu)
@@ -54,25 +62,25 @@ class MainView(tk.Frame):
 
         self.master.config(menu=self.menubar)
 
-
     def change_window(self, element, value):
-        """ View.main_view.MainView.change_window. 
+        """ View.main_view.MainView.change_window.
 
         Modifys the tkinter window's background color, font color or font size.
 
         Arguments:
             element: Dictates which display feature is changed
-            value: Is what the feature is changed to 
+            value: Is what the feature is changed to
         """
         mv_logger.debug('MainView.change_window')
 
         self.content_label[element] = value
 
     def build_window(self):
-        """ View.main_view.MainView.build_window sets the title of the window and the initial label
+        """ View.main_view.MainView.build_window sets the title of the
+        window and the initial label
 
-        Here the label is bound to a button that when clicked, will call open_article with the current
-        link as a parameter.
+        Here the label is bound to a button that when clicked, will
+        call open_article with the current link as a parameter.
         """
 
         mv_logger.debug('MainView.build_window')
@@ -87,16 +95,18 @@ class MainView(tk.Frame):
         self.pack()
 
     def display_entry(self, entry_title: str, entry_link: str):
-        """ Viw.main_view.MainView.display_entry changes the displayed title and associated link.
+        """
+        Viw.main_view.MainView.display_entry changes the displayed
+        title and associated link.
 
-        This function updates both entry_title and entry_link with the appropriate parameters and changes the text of
-        content_label to that of the new entry_title.
+        This function updates both entry_title and entry_link with the
+        appropriate parameters and changes the text ofcontent_label to
+        that of the new entry_title.
 
         Arguments:
             entry_title: a string showing a headline
             entry_link: a string that is the url for entry_title
         """
-
         mv_logger.debug('MainView.display_entry')
         self.entry_title = entry_title
         self.entry_link = entry_link
@@ -105,20 +115,24 @@ class MainView(tk.Frame):
         self.content_label.update()
 
     def open_article(self, link):
-        """ View.main_view.MainView.open_article opens the web page associated with the current entry_title
+        """
+        View.main_view.MainView.open_article opens the web page associated
+        with the current entry_title
 
         Arguments:
             link: url for the current entry_title
         """
-
         mv_logger.debug('MainView.open_article')
         webbrowser.open_new(link)
         self.content_label.update()
 
+
 def start_main_view():
-    """ View.main_view.start_main_view is called from controller.start and initiates our gui."""
-    
+    """
+    View.main_view.start_main_view is called from controller.start and
+    initiates our gui.
+    """
     mv_logger.debug('start_main_view')
-    
+
     root = tk.Tk()
     return MainView(master=root)
