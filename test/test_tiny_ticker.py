cd70feab380307b0c67c5a5a0d20bcfd51cbbd96
James Coleman <jcolem22@msudenver.edu>
2020-04-26 22:53:23 -0600

    Cleaned up test.test_tiny_ticker using pep257

--- test/test_tiny_ticker.py
+++ test/test_tiny_ticker.py
@@ -1,3 +1,4 @@
+"""Test.test_tiny_ticker."""
 import os
 import sys
 import unittest
@@ -9,15 +10,18 @@ from controller.tiny_ticker import main
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-class TestStart(unittest.TestCase):
+class TestTinyTicker(unittest.TestCase):
+    """Test class for controller.tiny_ticker.""" 
 
     @classmethod
     def setUp(cls):
+        """Test.test_tiny_ticker.TestTinyTicker.setUp."""
         root = tk.Tk()
         cls.test_view = MainView(master=root)
 
     @classmethod
     def tearDown(cls):
+        """Test.test_tiny_ticker.TestTinyTicker.tearDown."""
         cls.test_view.destroy()
 
 
@@ -25,7 +29,6 @@ class TestStart(unittest.TestCase):
     @patch('controller.tiny_ticker.parse')
     def test_call_ticker_argument_parser(self, mock_arg_parser, mock_parser):
         """Unit test for controller.tiny_ticker.main. Tests the call to the argument parser."""
-
         main(self.test_view)
         mock_arg_parser.assert_called()
 
@@ -42,7 +45,6 @@ class TestStart(unittest.TestCase):
     @patch('controller.tiny_ticker.parse')
     def test_call_ten_second_loop(self, mock_loop, mock_parser):
         """Unit test for controller.tiny_ticker.main. Tests the call to the ten second loop."""
-
         main(self.test_view)
         mock_loop.assert_called_with('www.fakeurl.com',)
 
