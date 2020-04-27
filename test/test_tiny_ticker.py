1956eff7357bc935cac8c1bf19b960caedadfed9
James Coleman <jcolem22@msudenver.edu>
2020-04-26 22:35:20 -0600

    changed test_all.md to TESTALL.md and I added set up and teardown class methods to test_tiny_ticker

--- test/test_tiny_ticker.py
+++ test/test_tiny_ticker.py
@@ -1,27 +1,36 @@
-import unittest
-from unittest.mock import patch
-from controller.tiny_ticker import main, ticker_argument_parser
-from view.main_view import MainView
-import tkinter as tk
 import os
 import sys
+import unittest
+import tkinter as tk
+from unittest.mock import patch
+from view.main_view import MainView
+
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class TestStart(unittest.TestCase):
 
+    @classmethod
+    def setUp(cls):
+        root = tk.Tk()
+        cls.test_view = MainView(master=root)
+
+    @classmethod
+    def tearDown(cls):
+        cls.test_view.destroy()
+
+
     @patch('controller.tiny_ticker.ticker_argument_parser')
     @patch('controller.tiny_ticker.parse')
     def test_call_ticker_argument_parser(self, mock_arg_parser, mock_parser):
-        """ Unit test for controller.tiny_ticker.main. Tests the call to the argument parser"""
+        """Unit test for controller.tiny_ticker.main. Tests the call to the argument parser."""
 
-        root = tk.Tk()
-        test_view = MainView(master=root)
         main(test_view)
         mock_arg_parser.assert_called()
 
     @patch('controller.tiny_ticker.parse')
     def test_call_parse(self, mock_parser):
-        """ Unit test for controller.tiny_ticker.main. Tests the call to the model's feedmanager.parse """
+        """Unit test for controller.tiny_ticker.main. Tests the call to the model's feedmanager.parse."""
         fake_url = ['www.fakeurl.com']
         sys.argv = ['test', '--url', fake_url[0]]
         root = tk.Tk()
@@ -32,7 +41,7 @@ class TestStart(unittest.TestCase):
     @patch('controller.tiny_ticker.ten_second_loop')
     @patch('controller.tiny_ticker.parse')
     def test_call_ten_second_loop(self, mock_loop, mock_parser):
-        """ Unit test for controller.tiny_ticker.main. Tests the call to the ten second loop """
+        """Unit test for controller.tiny_ticker.main. Tests the call to the ten second loop."""
 
         root = tk.Tk()
         test_view = MainView(master=root)
